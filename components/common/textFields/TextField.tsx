003db42a72701c8d6e95e5029040c21926737e7a
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-03-02 00:00:33 +0500

    Add textarea condition

--- components/common/textFields/TextField.tsx
+++ components/common/textFields/TextField.tsx
@@ -1,24 +1,26 @@
 import React, { InputHTMLAttributes } from 'react'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
     name: string,
-    label: string,
-    type?: string
+    label?: string,
+    type?: string,
+    multiline?: boolean,
+    rows?: number
 }
-
-const TextField = ({ name, label, type, ...rest }: InputProps) => {
+const style = "mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-[1px] py-2 px-3 border-gray-300 rounded-md"
+const TextField = ({ name, label, type, multiline, rows, ...rest }: InputProps) => {
     return (
         <div>
-            {label &&
-                <label htmlFor={name} className="block text-sm font-medium text-gray-700">
-                    {label}
-                </label>}
-            <input
-                type={type || "text"}
-                {...rest}
-                autoComplete="given-name"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-[1px] py-2 px-3 border-gray-300 rounded-md"
-            />
+
+            <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+                {label}
+            </label>
+            {multiline ? <textarea className={style} rows={rows}  {...rest} /> :
+                <input
+                    type={type || "text"}
+                    {...rest}
+                    className={style}
+                />}
         </div>
     )
 }
