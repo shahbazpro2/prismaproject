fc8873c2e01eba730478e568708622257014deaf
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-03-01 23:03:59 +0500

    Replace with TextField component

--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
+import TextField from './common/textFields/TextField'
 
 const UserInput = () => {
     return (
         <div>
-            <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                First name
-            </label>
-            <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-[1px] py-2 px-3 border-gray-300 rounded-md"
+            <h1 className='mb-10'>Create User</h1>
+            <TextField
+                label="Name"
+                name="name"
             />
         </div>
     )
