da2a008a3d16cb93970809566a5491fd8dd4d16f
Muhammad Shahbaz <shahbazrock0321@gmail.com>
2022-03-01 23:13:51 +0500

    Add more input field and button

--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import Button from './common/buttons/Button'
 import TextField from './common/textFields/TextField'
 
 const UserInput = () => {
     return (
         <div>
             <h1 className='mb-10'>Create User</h1>
-            <TextField
-                label="Name"
-                name="name"
-            />
+            <form className='space-y-5'>
+                <TextField
+                    label="Name"
+                    name="name"
+                />
+                <TextField
+                    label="Email"
+                    type="email"
+                    name="email"
+                />
+
+                <Button type="submit">Create</Button>
+            </form>
         </div>
     )
 }
